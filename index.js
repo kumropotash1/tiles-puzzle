872e22c6f18b1073366acc8e3eaa54e295f1aefd
kumropotash1 <arghyadeb.bandyopadhyay@gmail.com>
2023-03-06 01:24:06 +0530

    verify solvability of puzzle

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const rowCount = 4;
-const colCount = 4;
+const size = 4;
 
 class Tile {
-  currentRow = 0;
-  currentCol = 0;
+  render = true;
 
   constructor(originalRow, originalCol) {
     this.originalRow = originalRow;
     this.originalCol = originalCol;
   }
-
-  // set currentRow(value) {
-  //   this.currentRow = value;
-  // }
-
-  // set currentCol(value) {
-  //   this.currentCol = value;
-  // }
 }
 
 window.onload = () => {
@@ -28,8 +18,8 @@ window.onload = () => {
   canvas.width = 400;
   canvas.height = 400;
 
-  const tileWidth = canvas.width / colCount;
-  const tileHeight = canvas.height / rowCount;
+  const tileWidth = canvas.width / size;
+  const tileHeight = canvas.height / size;
 
   const ctx = canvas.getContext('2d');
 
@@ -39,29 +29,52 @@ window.onload = () => {
 
   img.src = "./squirrel.jpg";
 
-  img.onload = () => {
+  const newGame = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    /**
+     * @type {Tile[]} tiles
+     */
     const tiles = [];
 
-    for (let i = 0; i < rowCount; i++) {
-      for (let j = 0; j < colCount; j++) {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
         const t = new Tile(i, j);
 
-        if (i == rowCount - 1 && j == colCount - 1) {
-          t.originalCol = -1;
-          t.originalRow = -1;
+        if (i == size - 1 && j == size - 1) {
+          t.render = false;
         }
 
         tiles.push(t);
       }
     }
 
-    shuffle(tiles);
+    let solvable = false;
+
+    // Solvability of an n X n puzzle:
+    // https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+    while (!solvable) {
+      shuffle(tiles);
+
+      const t = [];
+
+      let emptyTileRow = 0;
+      tiles.forEach(tile => {
+        if (tile.render) t.push(tile.originalRow * size + tile.originalCol);
+        else emptyTileRow = tile.originalRow;
+      })
+
+      const inversions = inversionCount(t);
+
+      solvable = ((size % 2 === 1) && (inversions % 2 === 0)) || 
+      ((size % 2 === 0) && (inversions % 2 === size - emptyTileRow));
+
+      // console.log("Tiles: ", tiles, "Solvable: ", solvable);
+    }
 
     const tiles2d = tiles.reduce((accumulator, t, index) => {
-      const r = Math.floor(index / colCount);
-      const c = index % colCount;
+      const r = Math.floor(index / size);
+      const c = index % size;
 
       if (!Array.isArray(accumulator[r])) {
         accumulator[r] = [];
@@ -74,9 +87,9 @@ window.onload = () => {
 
     let emptyTileRow = 0, emptyTileCol = 0;
 
-    for (let i = 0; i < rowCount; i++) {
-      for (let j = 0; j < colCount; j++) {
-        if (tiles2d[i][j].originalCol >= 0 && tiles2d[i][j].originalRow >= 0) {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (tiles2d[i][j].originalCol >= 0 && tiles2d[i][j].originalRow >= 0 && tiles2d[i][j].render) {
           ctx.drawImage(img, tileWidth * tiles2d[i][j].originalCol, tileHeight * tiles2d[i][j].originalRow, tileWidth, tileHeight, (tileWidth + 1) * j, (tileHeight + 1) * i, tileWidth, tileHeight);
         } else {
           emptyTileRow = i;
@@ -88,7 +101,7 @@ window.onload = () => {
     const canvasLeft = canvas.clientLeft + canvas.offsetLeft;
     const canvasTop = canvas.clientTop + canvas.offsetTop;
 
-    const canvasHandler = (event) => {
+    const canvasClickHandler = (event) => {
       const clickX = event.clientX - canvasLeft;
       const clickY = event.clientY - canvasTop;
 
@@ -120,10 +133,10 @@ window.onload = () => {
           emptyTileRow = clickedRow;
           emptyTileCol = clickedColumn;
 
-          const victory = tiles2d.every((row, r) => row.every((tile, c) => (tile.originalRow === r || tile.originalRow=== -1) && (tile.originalCol === c || tile.originalCol === -1)))
+          const victory = tiles2d.every((row, r) => row.every((tile, c) => tile.originalRow === r && tile.originalCol === c))
 
           if (victory) {
-            canvas.removeEventListener('click', canvasHandler);
+            canvas.removeEventListener('click', canvasClickHandler);
 
             const p = new Text('Victory!');
             document.body.appendChild(p);
@@ -145,8 +158,10 @@ window.onload = () => {
       }
     }
 
-    canvas.addEventListener("click", canvasHandler)
+    canvas.addEventListener("click", canvasClickHandler)
   }
+
+  img.onload = newGame;
 }
 
 
@@ -166,4 +181,67 @@ function shuffle(array) {
   }
 
   return array;
+}
+
+/**
+ * 
+ * @param {number[]} arr 
+ */
+function inversionCount(arr) {
+  if (!Array.isArray(arr) || !arr.length) return 0;
+
+  function mergeSort(arr, left, right) {
+    let count = 0;
+
+    if (left < right) {
+      const mid = Math.floor((left + right) / 2);
+
+      count += mergeSort(arr, left, mid);
+      count += mergeSort(arr, mid + 1, right);
+      count += mergeAndCount(arr, left, mid, right);
+    }
+
+    return count;
+  }
+
+  /**
+   * 
+   * @param {number[]} arr
+   * @param {number} left
+   * @param {number} mid
+   * @param {number} right 
+   */
+  function mergeAndCount(arr, left, mid, right) {
+    const leftArr = [], rightArr = [];
+
+    for (let i = left; i <= mid; i++) {
+      leftArr.push(arr[i]);
+    }
+
+    for (let i = mid + 1; i <= right; i++) {
+      rightArr.push(arr[i]);
+    }
+
+    let i = 0, j = 0, k = left, inversions = 0;
+
+    while (i < leftArr.length && j < rightArr.length) {
+      if (leftArr[i] <= rightArr[j]) {
+        arr[k++] = leftArr[i++];
+      } else {
+        arr[k++] = rightArr[j++];
+        inversions += (mid + 1) - (left + i);
+      }
+    }
+
+    while (i < leftArr.length) {
+      arr[k++] = leftArr[i++];
+    }
+    while (j < rightArr.length) {
+      arr[k++] = rightArr[j++];
+    }
+
+    return inversions
+  }
+
+  return mergeSort(arr, 0, arr.length - 1);
 }
